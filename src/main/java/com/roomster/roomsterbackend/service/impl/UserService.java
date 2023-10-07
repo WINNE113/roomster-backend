25ebb1b6e39746243b71b2170192c72fe2292f03
WINNE113 <lehuuthang12a2thptcl@gmail.com>
2023-10-07 17:46:07 +0700

    Fit conflict

---
+++ src/main/java/com/roomster/roomsterbackend/service/impl/UserService.java
@@ -0,0 +1,19 @@
+package com.roomster.roomsterbackend.service.impl;
+
+import com.roomster.roomsterbackend.entity.UserEntity;
+import com.roomster.roomsterbackend.repository.UserRepository;
+import com.roomster.roomsterbackend.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService implements IUserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public Optional<UserEntity> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
