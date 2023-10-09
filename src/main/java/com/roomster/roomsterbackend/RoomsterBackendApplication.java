ffaecad7d8b3e17e74e345daf86c70745c5dd59a
WINNE113 <lehuuthang12a2thptcl@gmail.com>
2023-10-10 00:45:18 +0700

    modified file

--- src/main/java/com/roomster/roomsterbackend/RoomsterBackendApplication.java
+++ src/main/java/com/roomster/roomsterbackend/RoomsterBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties
 @SpringBootApplication()
 public class RoomsterBackendApplication {
-	public String POST = System.getenv("POST");
+	public String PORT = System.getenv("PORT");
 
 
 	public static void main(String[] args) {
