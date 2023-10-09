6bfe1fd9fca35f6347258e48700e0d2e60093a9b
WINNE113 <lehuuthang12a2thptcl@gmail.com>
2023-10-10 00:57:02 +0700

    fix deploy

--- src/main/java/com/roomster/roomsterbackend/RoomsterBackendApplication.java
+++ src/main/java/com/roomster/roomsterbackend/RoomsterBackendApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties
 @SpringBootApplication()
 public class RoomsterBackendApplication {
-	public String PORT = System.getenv("PORT");
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(RoomsterBackendApplication.class, args);
 	}
