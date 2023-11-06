6d36740cf330bc15f1280727717350e12f460891
anhhtgcd191331 <anhhtgcd191331@fpt.edu.vn>
2023-11-06 23:32:18 +0700

    just commit

---
+++ src/main/java/com/roomster/roomsterbackend/repository/PostTypeRepository.java
@@ -0,0 +1,13 @@
+package com.roomster.roomsterbackend.repository;
+
+import com.roomster.roomsterbackend.entity.PostEntity;
+import com.roomster.roomsterbackend.entity.PostTypeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public interface PostTypeRepository extends JpaRepository<PostTypeEntity, Long> {
+    PostTypeEntity getPostEntityByName(String postTypeName);
+}
