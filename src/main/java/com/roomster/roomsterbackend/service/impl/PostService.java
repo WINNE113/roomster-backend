fe1bbba2f007de7045701577953a722b23d0638f
WINNE113 <lehuuthang12a2thptcl@gmail.com>
2023-12-11 16:15:52 +0700

    Fix Images post

--- src/main/java/com/roomster/roomsterbackend/service/impl/PostService.java
+++ src/main/java/com/roomster/roomsterbackend/service/impl/PostService.java
@@ -71,29 +71,63 @@ public class PostService implements IPostService {
     }
     @Override
     public void upsertPost(PostDto postDTO, List<MultipartFile> images, Principal connectedUser) throws IOException {
-        PostEntity postEntity = postMapper.dtoToEntity(postDTO);
-        postEntity.setPostType(postTypeRepository.getPostEntityByCode(postDTO.getPost_type()));
-        postEntity.setDeleted(false);
-        // need admin or sp admin accept to APPROVED post
-        postEntity.setStatus(Status.REVIEW);
-        if(postDTO.getRotation() != null){
-            postEntity.setRotation(postDTO.getRotation());
-        }
+
         var user = (UserEntity)((UsernamePasswordAuthenticationToken)connectedUser).getPrincipal();
-        if(user != null){
-            postEntity.setAuthorId(user);
-        }
-        if (images != null && !images.isEmpty()) {
-            List<String> imageUrls = new ArrayList<>();
-            for (MultipartFile multipartFile : images) {
-                imageUrls.add(getFileUrls(multipartFile));
+        //TODO: Update post
+        if(postDTO.getPostId() != null && postDTO.getRoomDto().getInforRoomId() != null){
+            Optional<PostEntity> post =  postRepository.findById(postDTO.getPostId());
+            if(post.isPresent()){
+                postDTO.setImageUrlList(post.get().getImageUrlList());
+                post = Optional.of(postMapper.dtoToEntity(postDTO));
+                post.get().setPostType(postTypeRepository.getPostEntityByCode(postDTO.getPost_type()));
+                post.get().setDeleted(false);
+
+                // need admin or sp admin accept to APPROVED post
+                post.get().setStatus(Status.REVIEW);
+                if(postDTO.getRotation() != null){
+                    post.get().setRotation(postDTO.getRotation());
+                }
+
+                if(user != null){
+                    post.get().setAuthorId(user);
+                }
+                if (images != null && !images.isEmpty()) {
+                    List<String> imageUrls = new ArrayList<>();
+                    for (MultipartFile multipartFile : images) {
+                        imageUrls.add(getFileUrls(multipartFile));
+                    }
+                    post.get().setImageUrlList(imageUrls);
+                }
+                if(postDTO.getRoomDto() != null){
+                    post.get().setRoomId(inforRoomMapper.dtoToEntity(postDTO.getRoomDto()));
+                }
+                postRepository.save(post.get());
             }
-            postEntity.setImageUrlList(imageUrls);
+        }else {
+            //TODO: Add post
+            PostEntity postEntity = postMapper.dtoToEntity(postDTO);
+            postEntity.setPostType(postTypeRepository.getPostEntityByCode(postDTO.getPost_type()));
+            postEntity.setDeleted(false);
+            // need admin or sp admin accept to APPROVED post
+            postEntity.setStatus(Status.REVIEW);
+            if(postDTO.getRotation() != null){
+                postEntity.setRotation(postDTO.getRotation());
+            }
+            if(user != null){
+                postEntity.setAuthorId(user);
+            }
+            if (images != null && !images.isEmpty()) {
+                List<String> imageUrls = new ArrayList<>();
+                for (MultipartFile multipartFile : images) {
+                    imageUrls.add(getFileUrls(multipartFile));
+                }
+                postEntity.setImageUrlList(imageUrls);
+            }
+            if(postDTO.getRoomDto() != null){
+                postEntity.setRoomId(inforRoomMapper.dtoToEntity(postDTO.getRoomDto()));
+            }
+            postRepository.save(postEntity);
         }
-        if(postDTO.getRoomDto() != null){
-            postEntity.setRoomId(inforRoomMapper.dtoToEntity(postDTO.getRoomDto()));
-        }
-        postRepository.save(postEntity);
     }
 
     @Override
