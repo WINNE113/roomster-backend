666839618ed9c994c5b86510c04d9affbf16e4e5
WINNE113 <lehuuthang12a2thptcl@gmail.com>
2023-12-12 22:07:46 +0700

    Fix pageable history payment

--- src/main/java/com/roomster/roomsterbackend/repository/payment/PaymentRepository.java
+++ src/main/java/com/roomster/roomsterbackend/repository/payment/PaymentRepository.java
@@ -1,6 +1,7 @@
 package com.roomster.roomsterbackend.repository.payment;
 
 import com.roomster.roomsterbackend.entity.PaymentEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,5 @@ import java.util.List;
 @Repository
 public interface PaymentRepository extends JpaRepository<PaymentEntity, String> {
 
-    List<PaymentEntity> findAllByUserPayment_IdOrderByCreatedDate(Long userId);
+    List<PaymentEntity> findAllByUserPayment_IdOrderByCreatedDate(Long userId , Pageable pageable);
 }
